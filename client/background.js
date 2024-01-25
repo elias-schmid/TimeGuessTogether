33ec97e4c4b8bc032989608f1135fc17edce1ded
Elias Schmid <mail@elias-schmid.de>
2024-01-25 19:09:02 +0100

    migrate server connection to background

--- client/background.js
+++ client/background.js
@@ -1,6 +1,130 @@
 chrome.runtime.onInstalled.addListener(() => {
-      // Set the action badge to the next state
-      console.log("hello");
-
-    }
+    // Set the action badge to the next state
+    console.log("hello");
+}
 );
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    if (request.type !== undefined) {
+        switch (request.type) {
+            case "hostParty":
+                hostParty(sender.tab);
+                sendResponse({ status: "success" });
+                break;
+            case "joinParty":
+                if (request.code !== undefined) {
+                    socketConnectClient(sender.tab, request.code);
+                    sendResponse({ status: "success" });
+                } else {
+                    sendResponse({ status: "error", message: "no code provided" });
+                }
+                break;
+        }
+    }
+    return true;
+});
+
+
+var connectedClients = [];
+
+function sendMessageToTab(message, tabId, responseCallback = undefined) {
+    chrome.tabs.sendMessage(tabId, message, function (response) {
+        if (responseCallback !== undefined) {
+            responseCallback(response);
+        }
+    });
+}
+
+function hostParty(tab) {
+    var server_url = "https://" + getPartyServer();
+
+    fetch(server_url + "/api/hostParty", {
+        method: 'get',
+        mode: 'cors',
+        headers: new Headers({
+            'Content-Type': 'application/json'
+        })
+    })
+        .then(response => response.json())
+        .then(json => {
+            var party_code = json.data.party_code;
+            sendMessageToTab({ type: "loadLobbyScreen", asHost: true, code: party_code }, tab.id, (res) => {
+                if (res.status == "success") {
+                    socketConnectHost(tab, party_code);
+                }
+            });
+        });
+}
+
+function socketConnectHost(tab, code) {
+    const socket = new WebSocket("wss://" + getPartyServer());
+
+    socket.addEventListener("open", (event) => {
+        socket.send(JSON.stringify({ host: true, code: code }));
+    });
+
+    socket.addEventListener('message', (event) => {
+        msg = JSON.parse(event.data);
+        switch (msg.type) {
+            case "client_connect":
+                connectedClients.push({ id: msg.id, name: msg.name });
+                sendMessageToTab({ type: "updateConnectedClients", connectedClients: connectedClients }, tab.id);
+                break;
+            case "client_disconnect":
+                var index = connectedClients.findIndex((element) => element.id = msg.id);
+                connectedClients.splice(index, 1);
+                sendMessageToTab({ type: "updateConnectedClients", connectedClients: connectedClients }, tab.id);
+                break;
+        }
+    });
+
+    socket.addEventListener('close', (event) => {
+        var center_container = document.getElementsByClassName('centre')[0];
+        center_container.innerHTML = `<h1>Disconnected from Server.</h1>`;
+    });
+}
+
+function socketConnectClient(tab, code) {
+    const socket = new WebSocket("wss://" + getPartyServer());
+
+    socket.addEventListener("open", (event) => {
+        sendMessageToTab({ type: "getName" }, tab.id, (res) => {
+            if (res.status == "success" && res.name !== undefined) {
+                socket.send(JSON.stringify({ host: false, code: code, name: res.name }));
+            } else {
+                socket.send(JSON.stringify({ host: false, code: code, name: "undefined" }));
+            }
+        });
+    });
+
+    socket.addEventListener('message', (event) => {
+        msg = JSON.parse(event.data);
+        switch (msg.type) {
+            case "connection_success":
+                sendMessageToTab({ type: "loadLobbyScreen", asHost: false, code: code }, tab.id);
+                break;
+            case "client_disconnect":
+                var index = connectedClients.findIndex((element) => element.id = msg.id);
+                connectedClients.splice(index, 1);
+                sendMessageToTab({ type: "updateConnectedClients", connectedClients: connectedClients }, tab.id);
+                break;
+        }
+    });
+
+    socket.addEventListener('close', (event) => {
+        var center_container = document.getElementsByClassName('centre')[0];
+        center_container.innerHTML = `<h1>Disconnected from Host.</h1>`;
+    });
+}
+
+function getPartyServer() {
+    // var cookie_name = "party_server";
+    // var re = new RegExp(`^(?:.*;)?\\s*${cookie_name}\\s*=\\s*([^;]+)(?:.*)?$`);
+    // var value = (document.cookie.match(re) || [, null])[1];
+    // if (value === null) {
+    //     value = `${cookie_name}=${prompt("Enter Party Server (Format: <domain>:<port>)")}`;
+    //     document.cookie = value;
+    // }
+    // return value;
+    return "elias-schmid.de:11444"
+}
--- client/scripts/content.js
+++ client/scripts/content.js
@@ -4,7 +4,34 @@ partyLinkElement.style.cursor = "pointer";
 
 var linksWrap = document.getElementById('linksWrap');
 
-var connectedClients = [];
+function sendMessageToWorker(message, responseCallback = undefined) {
+    chrome.runtime.sendMessage(message, function (response) {
+        if (responseCallback !== undefined) {
+            responseCallback(response);
+        }
+    });
+}
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    if (request.type !== undefined) {
+        switch (request.type) {
+            case "loadLobbyScreen":
+                loadLobbyScreen(request.asHost, request.code);
+                sendResponse({ status: "success" });
+                break;
+            case "updateConnectedClients":
+                if (request.connectedClients !== undefined) {
+                    updateConnectedClients(request.connectedClients);
+                    sendResponse({ status: "success" });
+                }
+                break;
+            case "getName":
+                sendResponse({ status: "success", name: getName() });
+                break;
+        }
+    }
+    return true;
+});
 
 linksWrap.appendChild(partyLinkElement);
 
@@ -17,140 +44,58 @@ partyLinkElement.addEventListener("click", function (event) {
 
 function loadPartyPage() {
     var hostPartyElement = document.createElement("a");
-    hostPartyElement.innerHTML ="Host new Party";
+    hostPartyElement.innerHTML = "Host new Party";
     hostPartyElement.style.cursor = "pointer";
-    hostPartyElement.addEventListener("click", () => {hostParty();})
+    hostPartyElement.addEventListener("click", () => {
+        sendMessageToWorker({ type: "hostParty" });
+    });
     var joinPartyElement = document.createElement("a");
     joinPartyElement.innerHTML = "Join existing Party";
     joinPartyElement.style.cursor = "pointer";
-    joinPartyElement.addEventListener("click", () => {joinParty();})
+    joinPartyElement.addEventListener("click", () => {
+        var party_code = prompt("Enter Party Code:");
+        sendMessageToWorker({ type: "joinParty", code: party_code });
+    })
 
     linksWrap.innerHTML = "";
-    
+
     linksWrap.style.flexDirection = "column";
 
     linksWrap.appendChild(hostPartyElement);
     linksWrap.appendChild(joinPartyElement);
 }
 
-function hostParty() {
-    var server_url = "https://" + getPartyServer();
-
-    fetch(server_url+"/api/hostParty", {
-        method: 'get',
-        mode: 'cors',
-        headers: new Headers({
-            'Content-Type': 'application/json'
-        })
-    })
-    .then(response => response.json())
-    .then(json => {
-        var party_code = json.data.party_code;
-        var center_container = document.getElementsByClassName('centre')[0];
+function loadLobbyScreen(asHost, party_code) {
+    var center_container = document.getElementsByClassName('centre')[0];
+    if (asHost) {
         center_container.innerHTML = `<h1>Code: ${party_code}</h1><div id="connected_clients_container"></div>`;
-        updateConnectedClients();
-        socketConnectHost(party_code);
-    });
-
-}
-
-function socketConnectHost(code) {
-    const socket = new WebSocket("wss://" + getPartyServer());
-
-    socket.addEventListener("open", (event) => {
-        socket.send(JSON.stringify({host: true, code: code}));
-    });
-
-    socket.addEventListener('message', (event) => {
-        msg = JSON.parse(event.data);
-        switch (msg.type) {
-            case "connection_success":
-                var center_container = document.getElementsByClassName('centre')[0];
-                center_container.innerHTML = `<h1>Connected to ${msg.code}!</h1>`;
-                break;
-            case "client_connect":
-                connectedClients.push({id: msg.id, name: msg.name});
-                updateConnectedClients();
-                break;
-            case "client_disconnect":
-                var index = connectedClients.findIndex((element) => element.id = msg.id);
-                connectedClients.splice(index, 1);
-                updateConnectedClients();
-                break;
-
-        }
-    });
-
-    socket.addEventListener('close', (event) => {
-        var center_container = document.getElementsByClassName('centre')[0];
-        center_container.innerHTML = `<h1>Disconnected from Server.</h1>`;
-    });
-}
-
-
-function joinParty() {
-    var party_code = prompt("Enter Party Code:");
-    socketConnectClient(party_code);
-}
-
-function socketConnectClient(code) {
-    const socket = new WebSocket("wss://" + getPartyServer());
-
-    socket.addEventListener("open", (event) => {
-        socket.send(JSON.stringify({host: false, code: code, name: getName()}));
-    });
-
-    socket.addEventListener('message', (event) => {
-        msg = JSON.parse(event.data);
-        switch (msg.type) {
-            case "connection_success":
-                var center_container = document.getElementsByClassName('centre')[0];
-                center_container.innerHTML = `<h1>Joined ${msg.code}</h1><div id="connected_clients_container"></div>`;
-                break;   
-            case "client_disconnect":
-                var index = connectedClients.findIndex((element) => element.id = msg.id);
-                connectedClients.splice(index, 1);
-                updateConnectedClients();
-                break;
-        }
-    });
-
-    socket.addEventListener('close', (event) => {
-        var center_container = document.getElementsByClassName('centre')[0];
-        center_container.innerHTML = `<h1>Disconnected from Host.</h1>`;
-    });
-}
-
-function updateConnectedClients() {
-    var connectedClientsContainer = document.getElementById('connected_clients_container');
-    if(connectedClients.length == 0) {
-        connectedClientsContainer.innerHTML = "<h2>Waiting for other players...</h2>";
     } else {
-        connectedClientsContainer.innerHTML = "";
-        connectedClients.forEach((client) => {
-            connectedClientsContainer.innerHTML += `<h2>${client.name}</h2>`;
-        });
+        center_container.innerHTML = `<h1>Connected to ${party_code}!</h1><div id="connected_clients_container"></div>`;
     }
+    updateConnectedClients([]);
 }
 
-function getPartyServer() {
-    var cookie_name = "party_server";
-    var re = new RegExp(`^(?:.*;)?\\s*${cookie_name}\\s*=\\s*([^;]+)(?:.*)?$`);
-    var value = (document.cookie.match(re)||[,null])[1];
-    if(value === null) {
-        value = `${cookie_name}=${prompt("Enter Party Server (Format: <domain>:<port>)")}`;
-        document.cookie = value;
+function updateConnectedClients(connectedClients) {
+    var connectedClientsContainer = document.getElementById('connected_clients_container');
+    if(connectedClientsContainer !== null) {
+        if (connectedClients.length == 0) {
+            connectedClientsContainer.innerHTML = "<h2>Waiting for other players...</h2>";
+        } else {
+            connectedClientsContainer.innerHTML = "";
+            connectedClients.forEach((client) => {
+                connectedClientsContainer.innerHTML += `<h2>${client.name}</h2>`;
+            });
+        }
     }
-    return value;
 }
 
 function getName() {
     var login_name = document.getElementsByClassName("loginText");
-    if(login_name.length == 0) {
+    if (login_name.length == 0) {
         return "Anonymus";
     } else {
         login_name = login_name[0].innerHTML;
-        if(login_name == "Create Account") {
+        if (login_name == "Create Account") {
             return "Anonymus";
         }
         return login_name;
