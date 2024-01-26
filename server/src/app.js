6b1d943c682dbc9f78f7aff5ef2318c4aff4e8a2
Elias Schmid <mail@elias-schmid.de>
2024-01-26 20:32:58 +0000

    rewrite to use Maps instead of lists for connections

--- server/src/app.js
+++ server/src/app.js
@@ -7,6 +7,9 @@ const WebSocket = require('ws');
 const app = express();
 const port = 11444;
 
+const HOST = 0;
+const CLIENT = 1;
+
 const options = {
     key: fs.readFileSync('private.key'),
     cert: fs.readFileSync('cert.crt')
@@ -15,8 +18,8 @@ const options = {
 const server = https.createServer(options, app);
 const wss = new WebSocket.Server({ server });
 
-var hosts = [];
-var clients = [];
+var parties = new Map();
+var connections = new Map();
 var client_id_ctr = 0;
 
 app.use(express.json());
@@ -44,86 +47,93 @@ wss.on('connection', (ws) => {
         switch (res.type) {
             case "connect":
                 if (res.host == true) {
-                    hosts.push({
-                        code: res.code,
-                        socket: ws
-                    });
+                    parties.set(res.code, 
+                        {
+                            host: {socket: ws},
+                            clients: []
+                        });
+                    connections.set(ws, {type: HOST, code: res.code});
                     ws.send(JSON.stringify({ type: "message", data: "Successfully registered Party!" }));
-                    console.log(`Host connected. Currently are ${hosts.length} hosts connected.`);
+                    console.log(`Host connected.`);
                 } else {
-                    var host = hosts.find((curr) => curr.code == res.code);
-                    if (host !== undefined) {
+                    var party = parties.get(res.code);
+                    if (party !== undefined) {
                         var id = client_id_ctr++;
-                        host.socket.send(JSON.stringify({ type: "client_connect", id: id, name: res.name }))
-                        clients.push({
-                            code: res.code,
+                        party.host.socket.send(JSON.stringify({ type: "client_connect", id: id, name: res.name }));
+                        party.clients.push({
                             socket: ws,
                             name: res.name,
                             id: id
                         });
-                        ws.send(JSON.stringify({ type: "connection_success", code: host.code, id: id, name: res.name }));
+                        connections.set(ws, {type: CLIENT, code: res.code});
+                        ws.send(JSON.stringify({ type: "connection_success", code: party.host.code, id: id, name: res.name }));
                         console.log(`Client connected.`);
                     } else {
                         ws.send(JSON.stringify({ type: "message", data: "No such Party!" }));
-                        console.log(`Host not found.`);
+                        console.log(`Party not found.`);
                     }
                 }
                 break;
             case "startGame":
                 startGame((link) => {
                     ws.send(JSON.stringify({ type: "game_link", link: link }));
-                    var hostConnection = hosts.find((host) => host.socket == ws);
-                    clients.forEach((client) => {
-                        if (client.code == hostConnection.code) {
-                            try {
-                                client.socket.send(JSON.stringify({ type: "game_link", link: link }));
-                            } catch (error) {
-                                console.log(error);
-                            }
+                    var connection = connections.get(ws);
+                    var party = parties.get(connection.code);
+                    if(connection.type === HOST) {
+                        if(party !== undefined) {
+                            party.clients.forEach((client) => {
+                                try {
+                                    client.socket.send(JSON.stringify({ type: "game_link", link: link }));
+                                } catch (error) {
+                                    console.log(error);
+                                }
+                            });
+                        } else {
+                            console.log("Party not found");
                         }
-                    });
+                    } else {
+                        console.log("client is not allowed to start a game.");
+                    }
                 });
                 break;
         }
     });
 
     ws.on('close', () => {
-        var host_index = hosts.findIndex((element) => element.socket == ws);
-        if (host_index == -1) { // is client
-            var client_index = clients.findIndex((client) => client.socket == ws); // id of deleted client
-            if (client_index >= 0) {
-                var dead_client = clients[client_index]; // deleted client
-                var parent_index = hosts.findIndex((host) => host.code == dead_client.code); // id of parent of deleted client
-
-                try {
-                    hosts[parent_index].socket.send(JSON.stringify({ type: "client_disconnect", name: dead_client.name, id: dead_client.id }));
-                } catch (error) {
-                    console.log("Not possible to send to closed connection.");
-                }
-
-                clients.splice(client_index, 1);
-
-                clients.forEach((client) => {
-                    if (client.code == dead_client.code) {
+        var connection = connections.get(ws);
+        var party = parties.get(connection.code);
+        if(party !== undefined) {
+            if (connection.type === CLIENT) { // socket is client
+                var client_index = party.clients.findIndex((client) => client.socket == ws); // id of deleted client
+                if (client_index >= 0) {
+                    var dead_client = party.clients[client_index]; // deleted client
+    
+                    try {
+                        party.host.socket.send(JSON.stringify({ type: "client_disconnect", name: dead_client.name, id: dead_client.id }));
+                    } catch (error) {
+                        console.log("Not possible to send to closed connection.");
+                    }
+    
+                    party.clients.splice(client_index, 1);
+    
+                    party.clients.forEach((client) => {
                         try {
                             client.socket.send(JSON.stringify({ type: "client_disconnect", name: dead_client.name, id: dead_client.id }));
                         } catch (error) {
                             console.log(error);
                         }
-                    }
-                });
-            }
-            console.log(`Client disconnected.`);
-        } else {
-            clients.forEach((client, client_index) => {
-                if (client.code == hosts[host_index].code) {
-                    client.socket.close();
-                    clients.slice(client_index, 1);
+                    });
                 }
-            });
-            hosts.splice(host_index, 1);
-            console.log(`Host disconnected. ${hosts.length} hosts remaining.`);
+                console.log(`Client disconnected.`);
+            } else { // socket is host
+                party.clients.forEach((client, client_index) => {
+                    client.socket.close();
+                });
+                parties.delete(connection.code);
+                console.log(`Host disconnected.`);
+            }
         }
+        connections.delete(ws);
     });
 });
 
@@ -140,6 +150,12 @@ function startGame(callback = () => {}) {
         const shareLink = 'https://timeguessr.com/roundone?RA=' + String(json[5]) + ':' + String(json[6]);
         callback(shareLink);
     }).catch((error) => {console.log(error)});
+}
+
+function startTimer(durationInSeconds = 30, callback = () => {}) {
+    var timer = setTimeout(() => {
+        callback();
+    }, durationInSeconds * 1000);
 }
 
 server.listen(port, () => {
